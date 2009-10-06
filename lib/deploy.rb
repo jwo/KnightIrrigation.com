70062639f66fb9d35a47fddc872010062dfb8cc3
Jesse Wolgamott <jesse.wolgamott@gmail.com>
2009-10-05 21:33:21 -0500

    Break out services to multiple pages

---
+++ lib/deploy.rb
@@ -0,0 +1,38 @@
+set :application, "knightirrigation.com"
+set :user, "root" # I used root, less problems, but not recommended.
+ 
+set :scm, :git
+set :sudo, false
+default_run_options[:pty] = true
+ 
+# This distinction is necessary if the way you access github locally
+# is different from the way your production environment will access it.
+# For me it was the case.
+#set :local_repository, "git@github.com:jwo/KnightIrrigation.com.git"
+set :repository, "git@github.com:jwo/KnightIrrigation.com.git"
+ 
+set :deploy_to, "/u/apps/knightirrigation.com" # path to app on remote machine
+set :deploy_via, :remote_cache # quicker checkouts from github
+ 
+set :domain, 'knightirrigation.com' # your remote machine's domain name goes here
+role :app, "knightirrigation.com"
+role :web, "knightirrigation.com"
+ 
+set :runner, user
+set :admin_runner, runner
+ 
+namespace :deploy do
+  task :start, :roles => [:web, :app] do
+    deploy.start
+  end
+  
+  task :restart, :roles => [:web, :app] do    
+    run "cd touch #{deploy_to}/current/restart.txt"
+  end
+ 
+  # This will make sure that Capistrano doesn't try to run rake:migrate (this is not a Rails project!)
+  task :cold do
+    deploy.update
+    deploy.start
+  end
+end
--- navigation_lib.rb
+++ navigation_lib.rb
@@ -1,5 +1,5 @@
  def lib_nav_active(route, current_path)
     text = route
     route = '/' if route == "home"
-    "<li><a href=\"#{route}\"#{' class="active"' if route == current_path}>#{text}</a></li>"
+    "<li nowrap><a href=\"#{route}\"#{' class="active"' if route == current_path}>#{text}</a></li>"
   end
