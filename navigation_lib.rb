f8e927d44dbb98b734ef213333aed627ef207d17
Jesse Wolgamott <jwo@Jesse-Wolgamotts-MacBook-Pro.local>
2010-01-20 13:40:08 -0600

    Adding Sprinkler Repair page

--- navigation_lib.rb
+++ navigation_lib.rb
@@ -1,5 +1,5 @@
  def lib_nav_active(route, current_path)
     text = route
     route = '/' if route == "home"
-    "<li nowrap><a href=\"#{route}\"#{' class="active"' if route == current_path}>#{text}</a></li>"
+    "<li nowrap><a href=\"#{route}\"#{' class="active"' if route.gsub('-', ' ') == current_path.gsub('-', ' ')}>#{text.gsub('-', ' ').split(/\s+/).each{ |word| word.capitalize! }.join(' ') }</a></li>"
   end
